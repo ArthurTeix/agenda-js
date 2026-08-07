16b3e0bdf0d696bf4c80c2933d9730e79b2a1041
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-08-06 22:42:16 -0300

    feat: implemented login required

--- router.js
+++ router.js
@@ -5,6 +5,8 @@ const homeController = require('./src/controllers/homeController.js')
 const loginController = require('./src/controllers/loginController.js')
 const contatoController = require('./src/controllers/contatoController.js')
 
+const { loginRequired } = require('./src/middlewares/middleware.js') // garantir que o user tá logado
+
 // Rotas da home (index)
 route.get('/', homeController.index)
 
@@ -15,6 +17,6 @@ route.post('/login/login', loginController.login)
 route.get('/login/logout', loginController.logout)
 
 // Rotas de contato
-route.get('/contato/index', contatoController.index)
+route.get('/contato/index', loginRequired, contatoController.index) // garantir que apenas cadastrem contatos quem está logado
 
 module.exports = route  
--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -16,3 +16,13 @@ exports.csrfMiddleware = (req, res, next) => { // token da minha url
     res.locals.csrfToken = req.csrfToken()
     next()
 }
+
+exports.loginRequired = (req, res, next) => {
+    if (!req.session.user) {
+        req.flash('errors', 'Você precisa fazer login para cadastrar contatos')
+        req.session.save(() => res.redirect('/')) // sempre que redirecionar, é bom salvar a sessão para garantir que salvou
+        return
+    }
+
+    next()
+}
