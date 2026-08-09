6390b499ffbf610e124a7031d74cd74d772cd98d
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-08-09 19:27:21 -0300

    docs: add comments to clarify code logic

--- src/controllers/contatoController.js
+++ src/controllers/contatoController.js
@@ -1,18 +1,20 @@
+// Esse arquivo é responsável por renderizar minhas rotas e com os métodos e funções do model referente, garantir as funcionalidades e verificações da page
 const Contato = require('../models/ContatoModel.js')
 
 exports.index = (req, res) => {
     res.render('contato', {contato: {}})
 }
 
+// Parte responsável por listar os contatos
 // CREATE (crud)
-exports.register = async (req, res) => {
-    try {
-        const contato = new Contato(req.body)
-        await contato.register()
+exports.register = async (req, res) => { // toda função que vá fazer buscas no BD deve ser async
+    try { // sempre envolvido em try/catch para casos de erro
+        const contato = new Contato(req.body) // classe herdada de ContatoModel
+        await contato.register() // função herdada do model
     
-        if (contato.errors.length > 0) {
+        if (contato.errors.length > 0) { // se houver algum erro, deve ser emitido
             req.flash('errors', contato.errors)
-            req.session.save(function() {
+            req.session.save(function() { // salvando sempre a sessão para salvar os dados
                 return res.redirect('/contato/index')
             })
             return
@@ -23,19 +25,19 @@ exports.register = async (req, res) => {
             return res.redirect(`/`)
         })
         return
-    } catch (err) {
+    } catch (err) { // em caso de algum erro, deve me lançar o erro no console e render na 404
         console.log(err)
         return res.render('404')
     }
 }
 
 exports.editIndex = async (req, res) => {
-    if (!req.params.id) { return res.render('404') }
+    if (!req.params.id) { return res.render('404') } // se não tiver id, não pode editar (pois não existe)
 
-    const contato = await Contato.buscaPorId(req.params.id)
-    if (!contato) { return res.render('404') }
+    const contato = await Contato.buscaPorId(req.params.id) // busca no banco, logo é promises (await)
+    if (!contato) { return res.render('404') } 
 
-    res.render('contato', { contato })
+    res.render('contato', { contato }) 
 }
 
 // UPDATE (crud)
@@ -44,12 +46,12 @@ exports.edit = async (req, res) => {
         if (!req.params.id) { return res.render('404') }
     
         const contato = new Contato(req.body)
-        await contato.edit(req.params.id)
+        await contato.edit(req.params.id) // deve editar pelo id, para garantir identificação
     
         if (contato.errors.length > 0) {
             req.flash('errors', contato.errors)
             req.session.save(function() {
-                return res.redirect(`/contato/index/${req.params.id}`)
+                return res.redirect(`/contato/index/${req.params.id}`) // redirecionando para a mesma página em caso de erro, para facilitar, porém com flash de erro
             })
             return
         }
@@ -69,7 +71,7 @@ exports.edit = async (req, res) => {
 exports.delete = async (req, res) => {
     if (!req.params.id) { return res.render('404') }
 
-    const contato = await Contato.delete(req.params.id)
+    const contato = await Contato.delete(req.params.id) // deletando contato pelo id, para garantir
     if (!contato) { return res.render('404') }
 
     req.flash('success', 'Contato deletado com sucesso.')
